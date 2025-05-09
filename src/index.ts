4889a6bcda10582648f434d6ce0f562f7a858c4f
Charles Ferrell <charlesferrell@donutlabs.ai>
2025-05-09 17:34:26 -0400

    bump to newest version of solana-agent-kit
    Signed-off-by: Charles Ferrell <charlesferrell@donutlabs.ai>

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,38 @@
-import { ACTIONS, SolanaAgentKit , startMcpServer  } from "solana-agent-kit";
+import { SolanaAgentKit, KeypairWallet } from "solana-agent-kit";
+import { startMcpServer } from '@solana-agent-kit/adapter-mcp';
+import TokenPlugin from '@solana-agent-kit/plugin-token';
+import MiscPlugin from "@solana-agent-kit/plugin-misc";
+import { Keypair } from "@solana/web3.js";
 import * as dotenv from "dotenv";
+import bs58 from "bs58";
 
+// Load environment variables
 dotenv.config();
 
+ // Note this is a unused private key, do not use a real private key
+const keypair = Keypair.fromSecretKey(bs58.decode("3py5JaxcRQAP4McHFtoarMbsSKLgT9DaxqFGNhywT4qviZ9wFbz5N9e6P6tWdLqwpD3zaABnXcXSkiRKcaCay9ec"));
+const wallet = new KeypairWallet(keypair, process.env.RPC_URL! || "https://api.mainnet-beta.solana.com");
+
+// Create agent with plugin
 const agent = new SolanaAgentKit(
     // Note this is a unused private key, do not use a real private key
-    "3py5JaxcRQAP4McHFtoarMbsSKLgT9DaxqFGNhywT4qviZ9wFbz5N9e6P6tWdLqwpD3zaABnXcXSkiRKcaCay9ec",
+    wallet,
     process.env.RPC_URL! || "https://api.mainnet-beta.solana.com",
     {
-        COINGECKO_DEMO_API_KEY: process.env.COINGECKO_DEMO_API_KEY! || "",
+      COINGECKO_DEMO_API_KEY: process.env.COINGECKO_DEMO_API_KEY! || "",
     }
-);
+).use(TokenPlugin).use(MiscPlugin);
 
 
-// Note: commented out actions need a paid API key
-// Add your required actions here
-const mcp_actions = {
-    GET_ASSET: ACTIONS.GET_ASSET_ACTION,
-    GET_PRICE_INFERENCE: ACTIONS.GET_PRICE_INFERENCE_ACTION,
-    GET_TOKEN_PRICE_DATA: ACTIONS.GET_COINGECKO_TOKEN_PRICE_DATA_ACTION,
-    GET_TOKEN_INFO: ACTIONS.GET_COINGECKO_TOKEN_INFO_ACTION,
-    //GET_TOP_GAINERS: ACTIONS.GET_COINGECKO_TOP_GAINERS_ACTION,
-    GET_LATEST_POOLS: ACTIONS.GET_COINGECKO_LATEST_POOLS_ACTION,
-    GET_TRENDING_TOKENS: ACTIONS.GET_COINGECKO_TRENDING_TOKENS_ACTION,
-    GET_TRENDING_POOLS: ACTIONS.GET_COINGECKO_TRENDING_POOLS_ACTION,
-}
+console.log(agent.actions);
 
-startMcpServer(mcp_actions, agent, { name: "solana-agent", version: "0.0.1" });
+// Select which actions to expose to the MCP server
+const finalActions = {
+  TOKEN_DATA: agent.actions.find((action) => action.name === "GET_TOKEN_DATA")!,
+  GET_PRICE: agent.actions.find((action) => action.name === "FETCH_PRICE")!,
+  GET_RUGCHECK: agent.actions.find((action) => action.name === "RUGCHECK")!,
+  GET_TRENDING_TOKENS: agent.actions.find((action) => action.name === "GET_COINGECKO_TRENDING_TOKENS_ACTION")!,
+};
+
+// Start the MCP server
+startMcpServer(finalActions, agent, { name: "solana-agent", version: "0.0.1" });
